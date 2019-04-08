03a8a895cc55129925ebcc02e7afd901db9cbbe4
Dennis Aldana <dennis.aldana@interactuamovil.com>
2019-04-08 16:41:35 -0600

    Lesson 6

---
+++ Lesson 6 - Reflections/sphere.py
@@ -0,0 +1,51 @@
+from lib import *
+from dataclasses import dataclass
+
+WHITE = color(255, 255, 255)
+
+class Light(object):
+  def __init__(self, position=V3(0,0,0), intensity=1):
+    self.position = position
+    self.intensity = intensity
+
+class Material(object):
+  def __init__(self, diffuse=WHITE, albedo=(1, 0, 0), spec=0):
+    self.diffuse = diffuse
+    self.albedo = albedo
+    self.spec = spec
+
+class Intersect(object):
+  def __init__(self, distance, point, normal):
+    self.distance = distance
+    self.point = point
+    self.normal = normal
+
+class Sphere(object):
+  def __init__(self, center, radius, material):
+    self.center = center
+    self.radius = radius
+    self.material = material
+
+  def ray_intersect(self, orig, direction):
+    L = sub(self.center, orig)
+    tca = dot(L, direction)
+    l = length(L)
+    d2 = l**2 - tca**2
+    if d2 > self.radius**2:
+      return None
+    thc = (self.radius**2 - d2)**1/2
+    t0 = tca - thc
+    t1 = tca + thc
+    if t0 < 0:
+      t0 = t1
+    if t0 < 0:
+      return None
+
+    hit = sum(orig, mul(direction, t0))
+    normal = norm(sub(hit, self.center))
+
+    return Intersect(
+      distance=t0,
+      point=hit,
+      normal=normal
+    )
