d60bc508c8340906aa0c2f5591f814a5f1588862
denn1s <dennis.aldana@gmail.com>
2020-09-01 13:20:14 -0600

    Update sphere.py

--- Lesson 1 - Circles/sphere.py
+++ Lesson 1 - Circles/sphere.py
@@ -20,20 +20,3 @@ class Sphere(object):
     if t0 < 0:
       return False
     return True
-
-
-"""
-    bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
-        Vec3f L = center - orig;
-        float tca = L*dir;
-        float d2 = L*L - tca*tca;
-        if (d2 > radius*radius) return false;
-        float thc = sqrtf(radius*radius - d2);
-        t0       = tca - thc;
-        float t1 = tca + thc;
-        if (t0 < 0) t0 = t1;
-        if (t0 < 0) return false;
-        return true;
-    }
-};
-"""
